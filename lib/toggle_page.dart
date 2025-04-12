58ce38aa364cd2420bccb20348c6960047c5ce2c
Devbyaryan <aryantonk77@gmail.com>
2025-04-12 22:13:33 +0530

    toggle page done

--- lib/toggle_page.dart
+++ lib/toggle_page.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:git_learning_with_studio/bloc/toggle_bloc.dart';
 import 'package:git_learning_with_studio/bloc/toggle_event.dart';
-
-import 'bloc/toggle_state.dart';
+import 'package:git_learning_with_studio/bloc/toggle_state.dart';
 
 class TogglePage extends StatelessWidget {
   const TogglePage({super.key});
@@ -11,15 +11,41 @@ class TogglePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(create: (context) => ToggleBloc(),
-        child: BlocBuilder<ToggleBloc, ToggleState>(
-          builder: (context, state) {
-            return Center(
-              child: Switch(value: state.isOn, onChanged: (newValue){
-                context.read<ToggleBloc>().add(ToggleSwitchEvent());
-              }),
-            );
-          },
+      body: BlocProvider(
+        create: (_) => ToggleBloc(),
+        child: Center(
+          child: BlocBuilder<ToggleBloc, ToggleState>(
+            builder: (context, state) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Switch(
+                    value: state.isOn,
+                    onChanged: (newValue) {
+                      context.read<ToggleBloc>().add(ToggleSwitchEvent());
+                    },
+                    thumbIcon: WidgetStateProperty.resolveWith<Icon?>(
+                          (Set<WidgetState> states) {
+                        if (state.isOn) {
+                          return const Icon(Icons.check);
+                        } else {
+                          return const Icon(Icons.close);
+                        }
+                      },
+                    ),
+                    dragStartBehavior: DragStartBehavior.start,
+                  ),
+                  BlocBuilder<ToggleBloc, ToggleState>(
+                    builder: (context, state) {
+                      return Container(
+                          height: 200, width: 200, color: state.isOn ? Colors.red : Colors.blue
+                      );
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
